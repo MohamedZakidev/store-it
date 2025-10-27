dca391160ca4a642ba4b398f60ef76ea34d89488
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2025-10-27 17:27:22 +0300

    validating the form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,38 +4,145 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-const formSchemaForSignUp = z.object({
-  fullName: z.string().min(2).max(50),
-  email: z.email("Please enter a valid email address"),
-});
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { authFormProps, authFormType } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
-const formSchemaForSignIn = z.object({
-  email: z.email("Please enter a valid email address"),
-});
-
-type authFormProps = {
-  type: "sign-in" | "sign-up";
-};
+function generateAuthFormSchema(formType: authFormType) {
+  return z.object({
+    email: z.email("Please enter a valid email address"),
+    fullName:
+      formType === "sign-up"
+        ? z
+            .string()
+            .min(2, "Your name must be at least 2 characters long")
+            .max(50)
+        : z.string().optional(),
+  });
+}
 
 function AuthForm({ type }: authFormProps) {
+  // state
+  console.log("auth form render");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // form schema
+  const formSchema = generateAuthFormSchema(type);
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
-    resolver:
-      type === "sign-up"
-        ? zodResolver(formSchemaForSignUp)
-        : zodResolver(formSchemaForSignIn),
-    defaultValues:
-      type === "sign-up"
-        ? {
-            fullName: "",
-            email: "",
-          }
-        : {
-            email: "",
-          },
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      fullName: "",
+    },
   });
 
-  return <div>{type === "sign-in" ? "Sign In" : "Sign Up"}</div>;
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="form-title">
+            {type === "sign-up" ? "Create Account" : "Login"}
+          </h1>
+          {type === "sign-up" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">Full Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Enter your full Name"
+                        className="shad-input"
+                      />
+                    </FormControl>
+                  </div>
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
+          )}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Enter your email"
+                      className="shad-input"
+                    />
+                  </FormControl>
+                </div>
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            disabled={isLoading}
+            className="form-submit-button"
+          >
+            {type === "sign-in" ? "Sign In" : "Sign Up"}
+            {isLoading && (
+              <Image
+                src={"/assets/icons/loader.svg"}
+                alt="loader"
+                width={24}
+                height={24}
+                className="ml-2 animate-spin"
+              />
+            )}
+          </Button>
+          {errorMessage && <p className="error-message">*{errorMessage}</p>}
+          <div className="body-2 flex justify-center text-light-100">
+            {type === "sign-in" ? (
+              <p>
+                Don&apos;t have an account?{" "}
+                <span className="text-brand font-medium">
+                  <Link href={"/sign-up"}>Create Account</Link>
+                </span>
+              </p>
+            ) : (
+              <p>
+                Already have an account?{" "}
+                <span className="text-brand font-medium">
+                  <Link href={"/sign-in"}>Login</Link>
+                </span>
+              </p>
+            )}
+          </div>
+        </form>
+      </Form>
+      {/* otp verfication */}
+    </>
+  );
 }
 
 export default AuthForm;
---
+++ types/index.ts
@@ -0,0 +1,5 @@
+export type authFormType = "sign-in" | "sign-up";
+
+export type authFormProps = {
+  type: authFormType;
+};
